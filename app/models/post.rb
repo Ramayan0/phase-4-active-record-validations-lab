836c9774505f035c2683056a4a09ac526cdc0d55
hamsa <hafieekhan@gmail.com>
2022-11-25 12:32:36 +0300

    "Completed Lab"

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,22 @@
 class Post < ApplicationRecord
+  validates :title, presence: true
+  validates :content, length: { minimum: 250  }
+  validates :summary, length: { maximum: 250  }
+  validates :category, inclusion: {in: ['Fiction', 'Non-Fiction']}
+
+  validate :clickbait
+
+  Clickbait_Y =[
+    /Won't Believe/i,
+    /Secret/i,
+    /Top \d/i,
+    /Guess/i
+  ]
+
+  def clickbait
+    if Clickbait_Y.none? {|p| p.match title}
+      errors.add(:title, "Must be Clickbait")
+    end
+  end
 end
+
